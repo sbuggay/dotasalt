04ea176a16351f8fc9dafe276e123f59582dce47
Devan Buggay <sbuggay@gmail.com>
2014-08-21 11:07:31 -0500

    Reorganized package

--- dotasalt/__init__.py
+++ dotasalt/__init__.py
@@ -3,4 +3,5 @@ from flask import Flask
 app = Flask(__name__)
 
 import dotasalt.dotasalt
+import dotasalt.views
 import dotasalt.api
--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,4 @@ from werkzeug.contrib.fixers import ProxyFix
 from dotasalt import app
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
-app.run(debug=True, port=7070)
+app.run(debug=True, port=10001)
